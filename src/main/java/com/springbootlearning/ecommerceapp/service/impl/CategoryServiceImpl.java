bd9deff47878815da2946294030a5c526d2bd93d
AkshayHambir <hambirakshay379@gmail.com>
2026-03-15 19:21:29 +0530

    Updated category service methods to use persistent storage with h2 db

--- src/main/java/com/springbootlearning/ecommerceapp/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/springbootlearning/ecommerceapp/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.springbootlearning.ecommerceapp.service.impl;
 
 import com.springbootlearning.ecommerceapp.models.Category;
+import com.springbootlearning.ecommerceapp.repositories.CategoryRepository;
 import com.springbootlearning.ecommerceapp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -9,50 +11,43 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    private List<Category> categories = new ArrayList<>();
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public Category createCategory(Category category) {
-        if(Objects.isNull(category.getCategoryId())) {
-            if (categories.isEmpty()) {
-                category.setCategoryId(1L);
-            } else {
-                Long lastId = categories.get(categories.size() - 1).getCategoryId();
-                category.setCategoryId(lastId + 1);
-            }
-        }
-        categories.add(category);
-        return categories.get(categories.size() - 1);
+            return categoryRepository.save(category);
     }
 
     @Override
     public String updateCategory(Long categoryId, Category updatedCategory) {
+        List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         category.setCategoryName(updatedCategory.getCategoryName());
+        categoryRepository.save(category);
 
         return "Category with id: " + categoryId + " updated successfully";
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
-
-        categories.remove(category);
+        Optional<Category> categoryOptional = categoryRepository.findById(categoryId);
+        Category category = categoryOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+        categoryRepository.delete(category);
 
         return "Category with id : " + categoryId + " deleted successfully";
     }
