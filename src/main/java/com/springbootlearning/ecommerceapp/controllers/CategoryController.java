346f85c44cb5d2745e39c9af9c1c740ff5d2fdaf
AkshayHambir <hambirakshay379@gmail.com>
2026-04-05 11:53:35 +0530

    Code refactoring

--- src/main/java/com/springbootlearning/ecommerceapp/controllers/CategoryController.java
+++ src/main/java/com/springbootlearning/ecommerceapp/controllers/CategoryController.java
@@ -4,23 +4,18 @@ import com.springbootlearning.ecommerceapp.dto.category.CategoryInDTO;
 import com.springbootlearning.ecommerceapp.dto.category.CategoryOutDTO;
 import com.springbootlearning.ecommerceapp.dto.category.CategoryUpdateDTO;
 import com.springbootlearning.ecommerceapp.dto.response.PaginatedResponse;
-import com.springbootlearning.ecommerceapp.entities.CategoryEntity;
 import com.springbootlearning.ecommerceapp.service.CategoryService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
-
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @GetMapping("/public/categories")
     public ResponseEntity<PaginatedResponse<CategoryOutDTO>> getCategories(
@@ -28,14 +23,12 @@ public class CategoryController {
             @RequestParam(required = false, defaultValue = "10") Integer pageSize,
             @RequestParam(required = false, defaultValue = "categoryId") String sortBy,
             @RequestParam(required = false, defaultValue = "asc") String order
-    )
-    {
+    ) {
         return ResponseEntity.ok(categoryService.getCategories(pageNumber, pageSize, sortBy, order));
     }
 
     @PostMapping("/admin/categories")
-    public ResponseEntity<CategoryOutDTO> createCategory(@RequestBody @Valid CategoryInDTO categoryInDTO)
-    {
+    public ResponseEntity<CategoryOutDTO> createCategory(@RequestBody @Valid CategoryInDTO categoryInDTO) {
         CategoryOutDTO savedCategory = categoryService.createCategory(categoryInDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
     }
@@ -44,15 +37,14 @@ public class CategoryController {
     public ResponseEntity<CategoryOutDTO> updateCategory(
             @PathVariable Long categoryId,
             @RequestBody @Valid CategoryUpdateDTO categoryUpdateDTO
-    )
-    {
+    ) {
         CategoryOutDTO updatedCategory = categoryService.updateCategory(categoryId, categoryUpdateDTO);
-       return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
-    public void deleteCategory(@PathVariable Long categoryId)
-    {
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long categoryId) {
         categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
